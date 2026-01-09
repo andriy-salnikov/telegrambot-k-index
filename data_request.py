c88354bee2f0c3d693e8aa1a89600aa5c0efd3af
Andriy Salnikov <a.salnikov.home@gmail.com>
2026-01-09 13:54:24 +0100

    Obtain K-index using API

---
+++ data_request.py
@@ -0,0 +1,23 @@
+import requests
+from datetime import datetime, timedelta
+
+
+def get_data_from_api():
+    url = "https://services.swpc.noaa.gov/products/noaa-planetary-k-index-forecast.json"
+    response = requests.get(url)
+    json = response.json()
+    three_days_ago = (datetime.now() - timedelta(days=3)).strftime('%Y-%m-%d')
+    data = {}
+    for entry in json[1:]:
+        date = entry[0][:10]
+        value = round(float(entry[1]))
+        if date < three_days_ago:
+            continue
+        if date not in data or value > data[date]:
+            data[date] = value
+    return data
+
+
+if __name__ == "__main__":
+    get_data_from_api()
+    
